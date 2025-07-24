0f8f7236c03ebe2a24195ab1dcbdbc0dacf64629
Paul Toner <ptoner92@gmail.com>
2025-07-24 13:39:29 -0400

    Added aws env variables

--- amplify/functions/hello/resource.ts
+++ amplify/functions/hello/resource.ts
@@ -1,10 +1,17 @@
 import { defineFunction } from "@aws-amplify/backend";
 
+export const REGION = 'us-east-1';
+export const TABLE_NAME = 'products';
+
 export const sayHello = defineFunction({
     name: 'sayHello',
     entry: './handler.ts',
     bundling: {
         minify: false
+    },
+    environment: {
+        REGION,
+        TABLE_NAME
     }
     // handler: "index.handler",
     // events: [
