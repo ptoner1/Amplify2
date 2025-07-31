0a981ffa07b68b785b9511c8d2bad5f17cb48641
Paul Toner <ptoner92@gmail.com>
2025-07-31 14:41:48 -0400

    fetch Cognito User Details

--- src/Auth2.tsx
+++ src/Auth2.tsx
@@ -1,13 +1,17 @@
 import { Authenticator } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
+import { fetchUserAttributes } from "aws-amplify/auth";
+import { useEffect, useState } from "react";
 
 export const Auth = () => {
+  
     return (
         <Authenticator signUpAttributes={[
           'nickname'
         ]}>
             {({ signOut, user }) => (
               <main>
+                <UserDetails></UserDetails>
                 <h1>Hello {user?.username}</h1>
                 <button onClick={signOut}>Sign out</button>
               </main>
@@ -15,3 +19,15 @@ export const Auth = () => {
         </Authenticator>
     );
 }; 
+
+function UserDetails(){
+  const [nickName, setNickName] = useState('');
+  useEffect(() => {
+    async function getUserData(){
+      const userData = await fetchUserAttributes();
+      setNickName(userData.nickname ?? '');
+    }
+    getUserData();
+  }, []);
+  return <div><h1>Hello {nickName}</h1></div>
+}
